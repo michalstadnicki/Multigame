f9a45f3a85c2c311d9f798b11a9a18741f5ed30f
Michał Stadnicki <57849735+michalstadnicki@users.noreply.github.com>
2020-02-12 22:39:54 +0100

    First commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Game list:");
+        System.out.println("1 - game 1");
+        System.out.println("2 - game 2");
+        System.out.println("3 - game 3");
+        System.out.println("4 - game 4");
+        System.out.println("5 - game 5");
+    }
+
+}

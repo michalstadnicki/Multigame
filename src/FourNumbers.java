0ee7bfdc50e598eca5ea10997469e475984b6718
Michał Stadnicki <57849735+michalstadnicki@users.noreply.github.com>
2020-02-14 21:25:11 +0100

    Eleventh commit: add modification to 4 numbers game.

--- src/FourNumbers.java
+++ src/FourNumbers.java
@@ -16,15 +16,19 @@ public class FourNumbers {
 
         if (attemps < 1) {
             System.out.println("Wrong number attemps must be greater than 0");
-        }
-        else if (attemps > 20) {
+            FourNumbers numbers = new FourNumbers();
+            numbers.numbers();
+        } else if (attemps > 20) {
             System.out.println("Wrong number attemps must be lower than 20");
-        }
-
-        else {
+            FourNumbers numbers = new FourNumbers();
+            numbers.numbers();
+        } else {
+            System.out.println("");
             System.out.println("Computer generated 4 digits number (digits don't repeat).\n"
                     + "You can try to guess the 4 digits number in " + attemps + " attempts.\n");
-            System.out.println("***************************************\n");
+            System.out.println("***************************************");
+            System.out.println("**\t\t\t 4 NUMBERS GAME \t\t **");
+            System.out.println("***************************************");
 
             int[] random = numberGenerator();
 //        int attemps = 15;
@@ -49,65 +53,52 @@ public class FourNumbers {
                 } else {
                     attemps--;
                     if (attemps > 1) {
-                        System.out.print("You have guess " + indexMatch + " correct number in correct position," +
+                        System.out.println("You have guess " + indexMatch + " correct number in correct position," +
                                 " and " + match + " correct number in incorrect position. " + attemps + " attempts remaining. ");
                     } else if (attemps == 1) {
-                        System.out.print("You have guess " + indexMatch + " correct number in correct position," +
+                        System.out.println("You have guess " + indexMatch + " correct number in correct position," +
                                 " and " + match + " correct number in incorrect position. Last attempt! Good luck! ");
                     } else {
-                        System.out.print("Sorry, you failed. The number is: ");
+                        System.out.println("Sorry, you failed.");
+                        System.out.print("The number is: ");
                         for (int i = 0; i < random.length; i++) {
                             System.out.print(random[i]);
-
-                            System.out.print("Do you want play again 1 = YES, 2 = NO ");
-                            Scanner scanner2 = new Scanner(System.in);
-                            int answer = scanner2.nextInt();
-                            if (answer == 1 ) {
-                                FourNumbers numbers = new FourNumbers();
-                                numbers.numbers();
-                            }
-                            else {
-                                System.out.println("Thanks for playing this game !!!");
-                                Menu menu = new Menu();
-                                menu.menu();
-                            }
-
                         }
+
                     }
                 }
             }
         }
-
     }
 
-        public static int[] getGuess () {
-            Scanner keyboard = new Scanner(System.in);
-            System.out.print("Please enter your guess: ");
-            String input = keyboard.nextLine();
+    public static int[] getGuess() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your guess: ");
+        String input = keyboard.nextLine();
 
-            if (input.length() != 4 || input.replaceAll("\\D", "").length() != 4) {
-                System.out.println("Invalid number. You must enter 4 digits between 0-9 only.");
-                return getGuess();
-            }
-            int[] guess = new int[4];
-            for (int i = 0; i < 4; i++) {
-                guess[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
-            }
-            return guess;
+        if (input.length() != 4 || input.replaceAll("\\D", "").length() != 4) {
+            System.out.println("Invalid number. You must enter 4 digits between 0-9 only.");
+            return getGuess();
         }
-
-        public static int[] numberGenerator () {
-            Random random = new Random();
-            int[] randomArray = {10, 10, 10, 10};
-
-            for (int i = 0; i < randomArray.length; i++) {
-                int temp = random.nextInt(9);
-                while (temp == randomArray[0] || temp == randomArray[1] || temp == randomArray[2] || temp == randomArray[3]) {
-                    temp = random.nextInt(9);
-                }
-                randomArray[i] = temp;
-            }
-            return randomArray;
+        int[] guess = new int[4];
+        for (int i = 0; i < 4; i++) {
+            guess[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
         }
-
+        return guess;
     }
+
+    public static int[] numberGenerator() {
+        Random random = new Random();
+        int[] randomArray = {10, 10, 10, 10};
+
+        for (int i = 0; i < randomArray.length; i++) {
+            int temp = random.nextInt(9);
+            while (temp == randomArray[0] || temp == randomArray[1] || temp == randomArray[2] || temp == randomArray[3]) {
+                temp = random.nextInt(9);
+            }
+            randomArray[i] = temp;
+        }
+        return randomArray;
+    }
+
+}

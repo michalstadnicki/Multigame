9ceeb61daf0d8aa7ef884dc8734550a6c177eec8
Michał Stadnicki <57849735+michalstadnicki@users.noreply.github.com>
2020-02-14 03:29:33 +0100

    Fourth commit: change menu

--- src/App.java
+++ src/App.java
@@ -2,9 +2,6 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        Load load = new Load();
-        load.loading();
-
         Menu menu = new Menu();
         menu.menu();
 
--- src/Menu.java
+++ src/Menu.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public void menu() {
+    public void menu() throws InterruptedException {
 
-        System.out.println("Game list:");
-        System.out.println("1 - game 1");
-        System.out.println("2 - game 2");
-        System.out.println("3 - game 3");
-        System.out.println("4 - game 4");
-        System.out.println("5 - game 5");
-        System.out.println("Other number to exit");
-
-        System.out.println();
-
-        System.out.print("Enter: ");
+        System.out.println("***************************************");
+        System.out.println("*\t\t Game list:");
+        System.out.println("***************************************");
+        System.out.println("*\r [1] 4 numbers game");
+        System.out.println("*\r [2] Paper, rock, scissors game");
+        System.out.println("*\r [3] Dice game");
+        System.out.println("*\r [4] Tic Tac Toe game");
+        System.out.println("*\r [5] Black Jack game ");
+        System.out.println("***************************************");
+        System.out.println("***************************************");
+        System.out.println("ENTER CHOICE other number to exit:  ");
 
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
@@ -24,20 +24,35 @@ public class Menu {
             byte operationNumber = scanner.nextByte();
             switch (operationNumber) {
                 case 1:
+                    Load load1 = new Load();
+                    load1.loading();
                     System.out.println("game 1");
                     break;
+
                 case 2:
+                    Load load2 = new Load();
+                    load2.loading();
                     System.out.println("game 2");
                     break;
+
                 case 3:
+                    Load load3 = new Load();
+                    load3.loading();
                     System.out.println("game 3");
                     break;
+
                 case 4:
+                    Load load4 = new Load();
+                    load4.loading();
                     System.out.println("game 4");
                     break;
+
                 case 5:
+                    Load load5 = new Load();
+                    load5.loading();
                     System.out.println("game 5");
                     break;
+
                 default:
                     System.out.println("Thanks for playing");
                     exit = true;

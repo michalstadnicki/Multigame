5755b03ad01b90b79ff66446eb173885a3c6e4bd
Michał Stadnicki <57849735+michalstadnicki@users.noreply.github.com>
2020-02-14 17:41:56 +0100

    Ninth commit: add paper, rock, scissors game

---
+++ src/PaperRockScissors.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class PaperRockScissors {
+
+    public static void main(String[ ] args) {
+        
+        System.out.println("Paper Rock Scissors Game");
+        
+        Scanner scaner = new Scanner(System.in);
+        
+        System.out.println("[0] Rock \n" + "[1] Paper \n" + "[2] Scissors ");
+
+        System.out.print("ENTER CHOICE: ");
+        int user = scaner.nextInt();
+
+        Random random = new Random();
+        int computer = random.nextInt(2);
+
+        if (user==0) System.out.println("You have chosen ROCK");
+        if (user==1) System.out.println("You have chosen choosen PAPER");
+        if (user==2) System.out.println("You have chosen choosen SCISSORS");
+        if (computer==0) System.out.println("Computer has choosen ROCK");
+        if (computer==1) System.out.println("Computer has choosen PAPER");
+        if (computer==2) System.out.println("Computer has choosen SCISSORS");
+
+
+        if(computer==user)
+        {
+            System.out.println("It's a DRAW");
+            return;
+        }
+
+        if (computer==0){
+
+            if(user==1) {
+                System.out.println("You WIN");
+            }
+
+            else {
+                System.out.println("You LOSE");
+            }
+            return;
+
+        }
+        if (computer==1)
+        {
+            if (user==0) {
+                System.out.println("You LOSE!");
+            }
+            else {
+                System.out.println("You WIN");
+            }
+            return;
+        }
+
+        if (computer==2)
+        {
+            if (user==0) {
+                System.out.println("You WIN");
+            }
+             else {
+                 System.out.println("You LOSE");
+            }
+            return;
+        }
+    }
+
+}
